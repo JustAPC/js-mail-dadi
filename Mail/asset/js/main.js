835712455db25506d229869dabe06fa8be18279a
Andrea Pontillo <andreapontillo97@gmail.com>
2022-03-15 14:30:24 +0100

    inizio stesura esercizio mail

--- Mail/asset/js/main.js
+++ Mail/asset/js/main.js
@@ -1,7 +1,27 @@
+let utenteRegistrato = ["boolean@corso.it", "andreapontillo97@gmail.com", "andrea.pontillo@libero.it"]
 let mailUtente = prompt ("Qual'è la tua email?");
-
-const utenteRegistrato = ["boolean@corso.it", "andreapontillo97@gmail.com", "andrea.pontillo@libero.it"]
+console.log ("La mail dell'utente è " + mailUtente);
 
 for (let i = 0; i < utenteRegistrato.lenght; i++){
-    console.log (utenteRegistrato[i]);
+    
+    if (utenteRegistrato[i] == mailUtente) {
+        console.log("L'utente è registrato");
+        document.getElementById("risultato-verifica").innerHTML = "Sei già registrato al nostro sito!"
+
+    } else if (utenteRegistrato[i] != mailUtente) {
+        console.log("L'utente non è registrato")
+        document.getElementById("risultato-verifica").innerHTML = "Non sei ancora registrato al nostro sito"
+    }
+}
+
+let bottoneRegistrati = document.getElementById ("registrati-ora")
+
+if (mailUtente != utenteRegistrato){
+    document.getElementById("registrati-ora").className += " d-block";
+    
+    bottoneRegistrati.addEventListener("click",
+    function(){
+        utenteRegistrato.push (mailUtente);
+      }
+    );
 }
